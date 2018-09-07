b6efc4e44109331678c514ad9dd5ab3b4d1c48e9
Akshay <akshay.soni.8707@gmail.com>
2018-09-07 18:13:08 +0530

    JavaTest converted to Palindrome checker......

--- Java3/src/JavaTest.java
+++ Java3/src/JavaTest.java
@@ -1,5 +1,23 @@
+import java.util.Arrays;
+
 public class JavaTest {
     public static void main(String[] args) {
-        System.out.println("its me Akki......");
+        String number = "akka";
+        int start = 0, end, mid, i;
+        char data[] = number.toCharArray();
+        end = data.length;
+        --end;
+        mid = (end % 2 == 0) ? (end / 2) : (end + 1) / 2;
+        for (i = 0; i <= mid; ++i) {
+            if (i == mid) {
+                System.out.println(number + " is palindrome");
+            }
+            if (data[i] == data[end - i]) {
+                continue;
+            } else {
+                System.out.println(number + " is not palindrome");
+                break;
+            }
+        }
     }
 }
