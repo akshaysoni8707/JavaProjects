e7053257f7f1f81719289a65f2191851dd9e87ba
Akshay <akshay.soni.8707@gmail.com>
2018-09-08 14:16:34 +0530

    JavaProgramm2 convereted to generate all possible palindrome between two given numbers.......

--- Java3/src/JavaTest.java
+++ Java3/src/JavaTest.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-
 public class JavaTest {
     public static void main(String[] args) {
         String number = "akka";
--- java2/src/JavaProgramm2.java
+++ java2/src/JavaProgramm2.java
@@ -1,5 +1,26 @@
 public class JavaProgramm2 {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Integer number = 122;
+        int finalNumber = 1111;
+        do {
+            String s = number.toString();
+            int start = 0, end, mid, i;
+            char[] data;
+            data = s.toCharArray();
+            end = data.length;
+            --end;
+            mid = (end % 2 == 0) ? (end / 2) : (end + 1) / 2;
+            for (i = 0; i <= mid; ++i) {
+                if (i == mid) {
+                    System.out.println(number);
+                }
+                if (data[i] == data[end - i]) {
+                    continue;
+                } else {
+                    break;
+                }
+            }
+            number++;
+        } while (number < finalNumber);
     }
 }
