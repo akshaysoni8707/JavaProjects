9c449cfad1908f12667efdbdce2642861997d3d0
Akshay <akshay.soni.8707@gmail.com>
2019-05-08 15:35:08 +0530

    dynamic mapping and faking JSON type print of my own

--- experimentDatatypes/src/DynamicSelector.java
+++ experimentDatatypes/src/DynamicSelector.java
@@ -1,8 +1,10 @@
+import java.io.IOException;
 import java.util.LinkedHashMap;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.Serializable;
@@ -13,71 +15,103 @@ import java.util.Set;
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 class Template implements Serializable {
     public static final long serialVersionUID = 1L;
-
-    @JsonProperty(" ")
+    static int i = 0;
+    // @JsonProperty(" ")
+    String v = "";
     LinkedHashMap<String, Template> nest = new LinkedHashMap<>();
 
-
     public String loop(int n) {
-        StringBuffer sb1 = new StringBuffer();
+        StringBuilder sb1 = new StringBuilder();
         for (int i = 0; i < n; i++) {
             sb1.append("\t\t");
         }
         return sb1.toString();
     }
 
+    public void makeNest(String key) {
+        this.nest.putIfAbsent(key, new Template());
+    }
+
+    public Template getNest(String key) {
+        return this.nest.get(key);
+    }
+
+    public void putValue(String value) {
+        this.v = value;
+    }
+
     @Override
     public String toString() {
-//        StringBuffer sb = new StringBuffer();
-//        int i = 0;
-//        for (Map.Entry entry : nest.entrySet()) {
-//            i++;
-//            sb.append(loop(i-1)+"{").append(entry.getKey()).append(":\n").append(entry.getValue().toString()).append(loop(i-1)+"}\n");
-//        }
-//        return sb.toString();
+        StringBuilder sb = new StringBuilder();
+        int i1 = 0;
+        i++;
+        i1 = i;
+        if (!this.v.equals("")) {
+            sb.append("\"").append(this.v).append("\"");
+        }
+        for (Map.Entry entry : nest.entrySet()) {
+            //      i++;
+            //    sb.append(loop(i-  1)+"{").append(entry.getKey()).append(":\n").append(entry.getValue().toString()).append(loop(i-1)+"}\n");
+            sb.append("\n" + loop(i1) + "{").append("\"").append(entry.getKey()).append("\"").append(":").append(entry.getValue().toString()).append("\n" + loop(i1) + "}");
+        }
+        i--;
+        return sb.toString();
 
-        return "\t\n" + nest + "\t\n";
+/*        ObjectMapper objectMapper = new ObjectMapper();
+        String json = "";
+        try {
+
+            json = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(this);
+        } catch (Exception e) {
+            System.out.println("Json parssing error" + e.getMessage());
+        }
+        return json;*/
 
 
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        String json = "";
-//        try {
-//
-//            json = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(this.nest.entrySet().toArray());
-//        } catch (Exception e) {
-//            System.out.println("Json parssing error" + e.getMessage());
-//        }
-//        return json;
     }
+
 
 }
 
 public class DynamicSelector {
-    LinkedHashMap<String, Template> template = new LinkedHashMap<>();
-
-//    @Override
-//    public String toString() {
-//        return "DynamicSelector{" +
-//                "template=" + template +
-//                '}';
+//    public static void print(String jsonString){
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        Object object = null;
+//        try {
+//            object = objectMapper.readValue(jsonString, Object.class);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        System.out.println("JSON  mapping to Object with pretty print");
+//        try {
+//            System.out.println(objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(object));
+//        } catch (JsonProcessingException e) {
+//            e.printStackTrace();
+//        }
 //    }
 
     public static void main(String[] args) {
         Template template = new Template();
-        template.nest.putIfAbsent("India", new Template());
-        template.nest.get("India").nest.putIfAbsent("Gujarat", new Template());
-        template.nest.get("India").nest.putIfAbsent("Rajasthan", new Template());
-        template.nest.get("India").nest.putIfAbsent("Maharashtra", new Template());
-        template.nest.get("India").nest.get("Gujarat").nest.putIfAbsent("Surat", new Template());
-        template.nest.get("India").nest.get("Gujarat").nest.putIfAbsent("Navsari", new Template());
-        template.nest.get("India").nest.get("Gujarat").nest.putIfAbsent("Baroda", new Template());
-        template.nest.get("India").nest.get("Maharashtra").nest.putIfAbsent("Mumbai", new Template());
-        template.nest.get("India").nest.get("Maharashtra").nest.putIfAbsent("Pune", new Template());
-        template.nest.get("India").nest.get("Rajasthan").nest.putIfAbsent("Jaipur", new Template());
-        template.nest.get("India").nest.get("Rajasthan").nest.putIfAbsent("Udaipur", new Template());
-        template.nest.get("India").nest.get("Gujarat").nest.putIfAbsent("Description", new Template());
-        template.nest.get("India").nest.get("Gujarat").nest.get("Description").nest.putIfAbsent("dslkvzksldcvksvdslsdjkb,kfsiugfsugclzscglzsbc", new Template());
+        DynamicSelector sample = new DynamicSelector();
+        template.makeNest("India");
+        template.getNest("India").makeNest("Gujarat");
+        template.getNest("India").makeNest("Rajasthan");
+        template.getNest("India").makeNest("Maharashtra");
+        template.getNest("India").getNest("Gujarat").makeNest("Surat");
+        template.getNest("India").getNest("Gujarat").makeNest("Navsari");
+        template.getNest("India").getNest("Gujarat").makeNest("Baroda");
+        template.getNest("India").getNest("Maharashtra").makeNest("Mumbai");
+        template.getNest("India").getNest("Maharashtra").makeNest("Pune");
+        template.getNest("India").getNest("Rajasthan").makeNest("Jaipur");
+        template.getNest("India").getNest("Rajasthan").makeNest("Udaipur");
+        template.getNest("India").getNest("Gujarat").putValue("dslkvzksldcvksvdslsdjkb,kfsiugfsugclzscglzsbc");
+        template.makeNest("USA");
+        template.getNest("USA").makeNest("NewYork");
+        template.getNest("USA").getNest("NewYork").putValue("afdsxfbcb");
+        template.getNest("India").getNest("Gujarat").getNest("Surat").makeNest("River");
+        template.getNest("India").getNest("Gujarat").getNest("Surat").getNest("River").putValue("Tapi");
         System.out.println(template);
+        //     print(template.toString());
         //   template.disp();
     }
 }
